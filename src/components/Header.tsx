b77c3009c099ff918301c41c4edbcb73515c8e2e
RabGh <rabiighais@gmail.com>
2023-06-13 22:04:37 +0300

    header done, prettier rc config change, hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
+import React from "react"
+import { SocialIcon } from "react-social-icons"
+import { motion } from "framer-motion"
 
 type Props = {}
 
 function Header({}: Props) {
     return (
-        <header className="sticky top-0 z-20 mx-auto flex max-w-7xl items-start justify-between p-5 xl:items-center">
-            <div className="flex flex-row items-center">
+        <header
+            className="sticky top-0 z-20 mx-auto flex max-w-7xl 
+        items-start justify-between p-5 xl:items-center"
+        >
+            <motion.div
+                initial={{ x: -500, opacity: 0, scale: 0.5 }}
+                animate={{ x: 0, opacity: 1, scale: 1 }}
+                transition={{ duration: 1.5 }}
+                className="flex flex-row items-center"
+            >
                 <SocialIcon
                     url="https://twitter.com/RABBIToCOM"
                     target="_blank"
@@ -28,9 +37,14 @@ function Header({}: Props) {
                     fgColor="gray"
                     bgColor="transparent"
                 />
-            </div>
+            </motion.div>
 
-            <div className="flex cursor-pointer flex-row items-center text-gray-300">
+            <motion.div
+                initial={{ x: 500, opacity: 0, scale: 0.5 }}
+                animate={{ x: 0, opacity: 1, scale: 1 }}
+                transition={{ duration: 1.5 }}
+                className="flex cursor-pointer flex-row items-center text-gray-300"
+            >
                 <SocialIcon
                     className="cursor-pointer"
                     network="email"
@@ -42,7 +56,7 @@ function Header({}: Props) {
                 <p className="hidden text-sm uppercase text-gray-400 md:inline-flex">
                     Get In Touch
                 </p>
-            </div>
+            </motion.div>
         </header>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,31 @@
-import Image from "next/image";
-import Head from "next/head";
-import Header from "@/components/Header";
+import Image from "next/image"
+import Head from "next/head"
+import Header from "@/components/Header"
+import Hero from "@/components/Hero"
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Rabii Ghaith Portfolio Website</title>
-        <meta name="description" content="Rabii Ghaith Portfolio" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>Rabii Ghaith Portfolio Website</title>
+                <meta name="description" content="Rabii Ghaith Portfolio" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            {/* Hero */}
+            <section id="hero">
+                <Hero />
+            </section>
 
-      {/* Header */}
-      <Header />
-      {/* Hero */}
+            {/* About */}
 
-      {/* About */}
+            {/* Experiences */}
 
-      {/* Experiences */}
+            {/* Skills */}
 
-      {/* Skills */}
+            {/* Projects */}
 
-      {/* Projects */}
-
-      {/* Contact Me */}
-    </div>
-  );
+            {/* Contact Me */}
+        </div>
+    )
 }
