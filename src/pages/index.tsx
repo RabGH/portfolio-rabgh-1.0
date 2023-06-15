affa3398f027378ca1c75ce85e5d0fd3ceb1e050
RabGh <rabiighais@gmail.com>
2023-06-15 20:14:03 +0300

    deploy vercel path fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,6 @@ import { fetchExperiences } from "@utils/fetchExperiences";
 import { fetchSkills } from "@utils/fetchSkills";
 import { fetchProjects } from "@utils/fetchProjects";
 import { fetchSocials } from "@utils/fetchSocials";
-import axios from "axios";
 
 type Props = {
     pageInfo: PageInfo;
--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -4,7 +4,7 @@ export const fetchExperiences = async () => {
     const baseURL =
         process.env.NODE_ENV === "development"
             ? process.env.NEXT_PUBLIC_BASE_URL_DEV
-            : process.env.NEXT_PUBLIC_BASE_URL_PROD;
+            : process.env.VERCEL_URL;
 
     const res = await fetch(`${baseURL}/api/getExperiences`);
 
