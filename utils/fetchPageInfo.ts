0da7771ee3ffddde4316d292e4e9e698e084428a
RabGh <rabiighais@gmail.com>
2023-06-15 21:45:21 +0300

    process.env.VERCEL_URL I hope it works

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -4,7 +4,7 @@ export const fetchPageInfo = async () => {
     const baseURL =
         process.env.NODE_ENV === "development"
             ? process.env.NEXT_PUBLIC_BASE_URL_DEV
-            : `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getPageInfo`;
+            : `${process.env.VERCEL_URL}/api/getPageInfo`;
 
     const res = await fetch(`${baseURL}/api/getPageInfo`);
 
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -4,7 +4,7 @@ export const fetchSocials = async () => {
     const baseURL =
         process.env.NODE_ENV === "development"
             ? process.env.NEXT_PUBLIC_BASE_URL_DEV
-            :  `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/getSocials`;
+            : `${process.env.VERCEL_URL}/api/getSocials`;
 
     const res = await fetch(`${baseURL}/api/getSocials`);
 
