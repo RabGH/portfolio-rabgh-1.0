4ebca4c9bf2d62fcffe10a94d74d7b7182e1d1cd
RabGh <rabiighais@gmail.com>
2023-06-15 09:20:13 +0300

    utils folder path & fetch files for get files

---
+++ utils/fetchSocials.ts
@@ -0,0 +1,14 @@
+import { Social } from "@lib/types";
+
+export const fetchSocials = async () => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`
+  );
+
+  const data = await res.json();
+  const socials: Social[] = data.socials;
+
+  console.log("fetching", socials);
+
+  return socials;
+};
