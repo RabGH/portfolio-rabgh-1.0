94be9c928b54ffbbad9f2b3a0f19d3e2952f71d4
RabGh <rabiighais@gmail.com>
2023-06-15 22:38:34 +0300

    testing vercel deployment

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,9 +1,12 @@
 import { PageInfo } from "@lib/types";
 
-export const fetchPageInfo = async (context: any) => {
-    const pid = context.params.pid;
+export const fetchPageInfo = async () => {
+    const baseURL =
+        process.env.NODE_ENV === "development"
+            ? process.env.NEXT_PUBLIC_BASE_URL_DEV
+            : process.env.VERCEL_URL;
 
-    const res = await fetch(`${pid}/api/getPageInfo`);
+    const res = await fetch(`${baseURL}/api/getpageInfo`);
 
     const data = await res.json();
     const pageInfo: PageInfo = data.pageInfo;
