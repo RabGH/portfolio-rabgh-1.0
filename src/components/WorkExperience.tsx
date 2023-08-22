c3efb93a2d2db2786c3df4f5b4b15d79b682dcb0
RabGh <rabiighais@gmail.com>
2023-08-23 01:26:38 +0300

    Add arrow keys for experience cards scroll

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { motion } from "framer-motion";
 import ExperienceCards from "./ExperienceCards";
 import { Experience } from "@lib/types";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 
 type Props = {
     experiences: Experience[];
 };
 
 function WorkExperience({ experiences }: Props) {
+    const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
+    const scrollLeft = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollLeft -= 300;
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollLeft += 300;
+        }
+    };
+
     return (
         <motion.div
             initial={{ opacity: 0 }}
@@ -18,15 +33,26 @@ function WorkExperience({ experiences }: Props) {
             <h3 className="toptitle">Experience</h3>
 
             <div
-                className="flex w-full snap-x snap-mandatory space-x-5 
-                overflow-x-scroll p-10 scrollbar"
+                className="flex w-full snap-x snap-mandatory
+                overflow-x-scroll pt-10 scrollbar"
             >
-                {experiences?.map((experience) => (
-                    <ExperienceCards
-                        key={experience._id}
-                        experience={experience}
-                    />
-                ))}
+                <button className="" onClick={scrollLeft}>
+                    <FiChevronLeft className="animate-pulse" />
+                </button>
+                <div
+                    ref={scrollContainerRef}
+                    className="flex snap-x snap-mandatory overflow-x-scroll p-5 scrollbar"
+                >
+                    {experiences?.map((experience) => (
+                        <ExperienceCards
+                            key={experience._id}
+                            experience={experience}
+                        />
+                    ))}
+                </div>
+                <button className="" onClick={scrollRight}>
+                    <FiChevronRight className="animate-pulse" />
+                </button>
             </div>
         </motion.div>
     );
